da09d81c071aeddc6cb8c21e48e0e326a3578fcc
Kazuma Arino <hogeika2@gmail.com>
2021-12-01 11:50:00 +0900

    update

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,6 +3,9 @@
 SUBWIKI_NAME="RandomThoughts"
 
 SCRIPTS_DIR=$(dirname $0)
-DEST="${SCRIPTS_DIR}/../wiki_src"
+DEST_SRC="${SCRIPTS_DIR}/../wiki_src"
+DEST_RES="${SCRIPTS_DIR}/../wiki"
 
-cp ~/GoogleDriveMirror/DriveText/TeFWiki/${SUBWIKI_NAME}/*.md $DEST/
+rm $DEST_SRC/*
+rm $DEST_RES/*
+cp ~/GoogleDriveMirror/DriveText/TeFWiki/${SUBWIKI_NAME}/*.md $DEST_SRC/
