04fd5600cb593054345cc08f9d19887194145fa6
Kazuma Arino <hogeika2@gmail.com>
2023-04-17 16:26:48 +0900

    update

--- scripts/gen_recents.sh
+++ scripts/gen_recents.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ls -t ../wiki_src/*.md | xargs -P 20 -L 1 ./gen_pair.sh | sort -r | head -n 15 | cut -b 24- | awk -f gen_recents.awk > ../_includes/git-wiki/components/lists/gened_recents.html
+ls -t ../wiki_src/*.md | xargs -P 20 -L 1 ./gen_pair.sh | sort -r -s | head -n 15 | cut -b 24- | awk -f gen_recents.awk > ../_includes/git-wiki/components/lists/gened_recents.html
