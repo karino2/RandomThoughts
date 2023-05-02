6213bab58fb03c7dd4b192563bc278ff67d65488
Kazuma Arino <hogeika2@gmail.com>
2023-05-02 12:21:58 +0900

    recents encode test.

--- scripts/gen_recents.py
+++ scripts/gen_recents.py
@@ -1,11 +1,13 @@
 import sys
 import unicodedata
+import urllib.parse
 
 
 for line in sys.stdin:
     line = unicodedata.normalize('NFC', line)
     wikiname = line[:-4]
+    encoded = urllib.parse.quote(wikiname, safe='')
     print("        <li class=\"page-list-item\">")
-    print("            <a href=\"{{ \"/%s\" | relative_url }}\">{{\"%s\" | escape}}</a>" % (wikiname, wikiname))
+    print("            <a href=\"{{ \"/%s\" | relative_url }}\">{{\"%s\" | escape}}</a>" % (encoded, wikiname))
     print("        </li>")
 
