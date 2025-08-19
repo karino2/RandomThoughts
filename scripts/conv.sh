ccf606c5cfe0655a9e231af1c2f354efe320a3fd
Kazuma Arino <hogeika2@gmail.com>
2025-08-19 10:06:26 +0900

    update

--- scripts/conv.sh
+++ scripts/conv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
 ls ../wiki_src/*.md | xargs -P 20 -L 1 ./conv_one.sh
+cp -r ../wiki_src/imgs ../wiki/
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -6,6 +6,9 @@ SCRIPTS_DIR=$(dirname $0)
 DEST_SRC="${SCRIPTS_DIR}/../wiki_src"
 DEST_RES="${SCRIPTS_DIR}/../wiki"
 
+rm -r $DEST_SRC/imgs
+rm -r $DEST_RES/imgs
 rm $DEST_SRC/*
 rm $DEST_RES/*
 cp  ~/work/syncthing_dirs/TeFWiki/${SUBWIKI_NAME}/*.md $DEST_SRC/
+cp  -r ~/work/syncthing_dirs/TeFWiki/${SUBWIKI_NAME}/imgs $DEST_SRC/
