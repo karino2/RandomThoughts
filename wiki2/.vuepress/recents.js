e637f1fe7a527bf2b0df49666708aaae13107eae
Kazuma Arino <hogeika2@gmail.com>
2025-10-01 22:55:16 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,13 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%83%8F%E3%83%BC%E3%83%95%E3%83%88%E3%83%BC%E3%83%B3.html", "text": "ハーフトーン"},
+    {"link": "%E3%82%B5%E3%83%96Wiki%E3%81%AE%E5%85%AC%E9%96%8B%E6%89%8B%E9%A0%86.html", "text": "サブWikiの公開手順"},
+    {"link": "VuePress.html", "text": "VuePress"},
+    {"link": "Stippling.html", "text": "Stippling"},
+    {"link": "MFG.html", "text": "MFG"},
     {"link": "Home.html", "text": "Home"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
-    {"link": "VuePress.html", "text": "VuePress"},
     {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "JavaScript.html", "text": "JavaScript"},
@@ -12,10 +16,6 @@ export const recents = [
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%81%9F%E3%81%A6%E3%81%AA%E3%81%8B%E6%B5%81%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "たてなか流クイックスケッチ"},
     {"link": "%E9%A3%9F%E4%BA%8B%E9%96%A2%E9%80%A3.html", "text": "食事関連"},
-    {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
-    {"link": "%E3%82%AA%E3%83%BC%E3%83%88%E3%83%9F%E3%83%BC%E3%83%AB.html", "text": "オートミール"},
-    {"link": "Dio110.html", "text": "Dio110"},
-    {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
    ]
 }
 ]
