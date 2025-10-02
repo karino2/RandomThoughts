7f43688ca08beef64c5ed6a046345c9f623204fe
Kazuma Arino <hogeika2@gmail.com>
2025-10-02 15:07:52 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,12 +1,12 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
     {"link": "VuePress.html", "text": "VuePress"},
+    {"link": "MFG.html", "text": "MFG"},
+    {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
     {"link": "%E3%83%8F%E3%83%BC%E3%83%95%E3%83%88%E3%83%BC%E3%83%B3.html", "text": "ハーフトーン"},
     {"link": "%E3%82%B5%E3%83%96Wiki%E3%81%AE%E5%85%AC%E9%96%8B%E6%89%8B%E9%A0%86.html", "text": "サブWikiの公開手順"},
     {"link": "Stippling.html", "text": "Stippling"},
-    {"link": "MFG.html", "text": "MFG"},
     {"link": "Home.html", "text": "Home"},
     {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
