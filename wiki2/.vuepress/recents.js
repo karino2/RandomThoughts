4294678fb85e9dbcc6883148baeced64ee3c4318
Kazuma Arino <hogeika2@gmail.com>
2025-10-04 16:05:49 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
     {"link": "LenovoTabP12.html", "text": "LenovoTabP12"},
+    {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
     {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "Cpp.html", "text": "Cpp"},
     {"link": "VuePress.html", "text": "VuePress"},
