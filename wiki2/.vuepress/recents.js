725b3a7d224f2d10b552d75e7bcd8e636b4f1be4
Kazuma Arino <hogeika2@gmail.com>
2025-10-04 22:05:42 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,13 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%83%8E%E3%83%BC%E3%83%88PC%E7%94%A8%E9%9B%BB%E6%BA%90.html", "text": "ノートPC用電源"},
+    {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
+    {"link": "USB%E5%85%85%E9%9B%BB%E5%99%A8.html", "text": "USB充電器"},
+    {"link": "LenovoTabP12.html", "text": "LenovoTabP12"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "PDF.html", "text": "PDF"},
     {"link": "Android.html", "text": "Android"},
-    {"link": "LenovoTabP12.html", "text": "LenovoTabP12"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
     {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "Cpp.html", "text": "Cpp"},
@@ -13,9 +16,6 @@ export const recents = [
     {"link": "%E3%83%8F%E3%83%BC%E3%83%95%E3%83%88%E3%83%BC%E3%83%B3.html", "text": "ハーフトーン"},
     {"link": "%E3%82%B5%E3%83%96Wiki%E3%81%AE%E5%85%AC%E9%96%8B%E6%89%8B%E9%A0%86.html", "text": "サブWikiの公開手順"},
     {"link": "Stippling.html", "text": "Stippling"},
-    {"link": "Home.html", "text": "Home"},
-    {"link": "GithubPages.html", "text": "GithubPages"},
-    {"link": "JavaScript.html", "text": "JavaScript"},
    ]
 }
 ]
