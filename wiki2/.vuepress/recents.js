f4cee0d7cf8e6e1af3a233f91b6f85eff6723625
Kazuma Arino <hogeika2@gmail.com>
2025-10-05 15:06:00 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E5%9B%B2%E7%A2%81.html", "text": "囲碁"},
+    {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88PC%E7%94%A8%E9%9B%BB%E6%BA%90.html", "text": "ノートPC用電源"},
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
@@ -10,12 +12,10 @@ export const recents = [
     {"link": "PDF.html", "text": "PDF"},
     {"link": "Android.html", "text": "Android"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月上旬の雑記"},
-    {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "Cpp.html", "text": "Cpp"},
     {"link": "VuePress.html", "text": "VuePress"},
     {"link": "MFG.html", "text": "MFG"},
     {"link": "%E3%83%8F%E3%83%BC%E3%83%95%E3%83%88%E3%83%BC%E3%83%B3.html", "text": "ハーフトーン"},
-    {"link": "%E3%82%B5%E3%83%96Wiki%E3%81%AE%E5%85%AC%E9%96%8B%E6%89%8B%E9%A0%86.html", "text": "サブWikiの公開手順"},
    ]
 }
 ]
