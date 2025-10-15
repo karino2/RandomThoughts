5f2b400d3c5c5f14390dbcd4b275809424e0c964
Kazuma Arino <hogeika2@gmail.com>
2025-10-15 12:05:42 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%81%A6%E3%81%8D%E3%81%99%E3%81%A8TL.html", "text": "てきすとTL"},
     {"link": "htmnix.html", "text": "htmnix"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E4%B8%80%E4%BA%BA%E3%81%A7%E4%BD%BF%E3%81%86%E3%82%BF%E3%82%A4%E3%83%A0%E3%83%A9%E3%82%A4%E3%83%B3%E7%9A%84%E3%81%AA%E4%BD%95%E3%81%8B%E3%81%8C%E6%AC%B2%E3%81%97%E3%81%84.html", "text": "一人で使うタイムライン的な何かが欲しい"},
     {"link": "%E3%81%A6%E3%81%8D%E3%81%99%E3%81%A8%E3%81%A7%E3%81%A3%E3%81%8D.html", "text": "てきすとでっき"},
-    {"link": "%E3%81%A6%E3%81%8D%E3%81%99%E3%81%A8TL.html", "text": "てきすとTL"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月中旬の雑記"},
     {"link": "USB%E5%85%85%E9%9B%BB%E5%99%A8.html", "text": "USB充電器"},
     {"link": "VSCode.html", "text": "VSCode"},
