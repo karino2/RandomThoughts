5b9f462d9d272b978966d21ce3ce25f9e3320b12
Kazuma Arino <hogeika2@gmail.com>
2025-10-16 18:06:31 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E5%9B%B2%E7%A2%81%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%B3%E8%A9%95%E4%BE%A1.html", "text": "囲碁エンジン評価"},
+    {"link": "%E5%9B%B2%E7%A2%81.html", "text": "囲碁"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月中旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "%E7%B5%8C%E6%B8%88.html", "text": "経済"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E4%B8%80%E4%BA%BA%E3%81%A7%E4%BD%BF%E3%81%86%E3%82%BF%E3%82%A4%E3%83%A0%E3%83%A9%E3%82%A4%E3%83%B3%E7%9A%84%E3%81%AA%E4%BD%95%E3%81%8B%E3%81%8C%E6%AC%B2%E3%81%97%E3%81%84.html", "text": "一人で使うタイムライン的な何かが欲しい"},
     {"link": "%E3%81%A6%E3%81%8D%E3%81%99%E3%81%A8%E3%81%A7%E3%81%A3%E3%81%8D.html", "text": "てきすとでっき"},
-    {"link": "USB%E5%85%85%E9%9B%BB%E5%99%A8.html", "text": "USB充電器"},
-    {"link": "VSCode.html", "text": "VSCode"},
    ]
 }
 ]
