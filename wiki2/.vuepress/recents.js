b7df95b44950f3c6402d61864edffa8bfc97eb3d
Kazuma Arino <hogeika2@gmail.com>
2025-10-18 11:06:28 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "FDroid.html", "text": "FDroid"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月中旬の雑記"},
     {"link": "Qt.html", "text": "Qt"},
-    {"link": "FDroid.html", "text": "FDroid"},
     {"link": "%E5%9B%B2%E7%A2%81%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%B3%E8%A9%95%E4%BE%A1.html", "text": "囲碁エンジン評価"},
     {"link": "%E5%9B%B2%E7%A2%81.html", "text": "囲碁"},
     {"link": "%E7%B5%8C%E6%B8%88.html", "text": "経済"},
