55d04b9d19819d7ef6954e7da924211a45eef694
Kazuma Arino <hogeika2@gmail.com>
2025-10-19 13:33:46 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "FDroid.html", "text": "FDroid"},
     {"link": "2025%E5%B9%B410%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年10月中旬の雑記"},
     {"link": "%E3%81%9F%E3%81%A6%E3%81%AA%E3%81%8B%E6%B5%81%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "たてなか流クイックスケッチ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "TeFWiki.html", "text": "TeFWiki"},
-    {"link": "FDroid.html", "text": "FDroid"},
     {"link": "USB%E5%85%85%E9%9B%BB%E5%99%A8.html", "text": "USB充電器"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "Qt.html", "text": "Qt"},
