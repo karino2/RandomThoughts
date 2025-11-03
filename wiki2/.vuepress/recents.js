1a6769741c929bf64b74222968021d4f14a7baa7
Kazuma Arino <hogeika2@gmail.com>
2025-11-03 21:05:40 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88PC.html", "text": "ノートPC"},
     {"link": "MacBookAir.html", "text": "MacBookAir"},
     {"link": "Mac.html", "text": "Mac"},
-    {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "%E3%82%AC%E3%82%B9%E3%83%95%E3%82%A1%E3%83%B3%E3%83%92%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "ガスファンヒーター"},
