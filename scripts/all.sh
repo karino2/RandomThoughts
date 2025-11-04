c3fc1d293fc34c49c0a0774930d9a9b38b4426d6
Kazuma Arino <hogeika2@gmail.com>
2025-11-04 10:45:01 +0900

    Parametrize copy src.

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pushd $(dirname $0);
-./copy.sh;
+./copy.sh $1;
 pushd ../; git add --all; git commit -m "update"; popd; ./gen_recents.sh; pushd ../; git add --all; git commit -m "update recents";  git push; popd;
 popd;
 
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,9 +3,10 @@
 SUBWIKI_NAME="RandomThoughts"
 
 SCRIPTS_DIR=$(dirname $0)
+TEFWIKI_DIR=$1/TeFWiki
 DEST_SRC="${SCRIPTS_DIR}/../wiki_src"
 
 rm -r $DEST_SRC/imgs
 rm $DEST_SRC/*
-cp  ~/work/syncthing_dirs/TeFWiki/${SUBWIKI_NAME}/*.md $DEST_SRC/
-cp  -r ~/work/syncthing_dirs/TeFWiki/${SUBWIKI_NAME}/imgs $DEST_SRC/
+cp  ${TEFWIKI_DIR}/${SUBWIKI_NAME}/*.md $DEST_SRC/
+cp  -r ${TEFWIKI_DIR}/${SUBWIKI_NAME}/imgs $DEST_SRC/
