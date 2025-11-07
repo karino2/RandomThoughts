2017113c50348e1a9bbfdd6b9bc0ebab84cb08f0
Kazuma Arino <hogeika2@gmail.com>
2025-11-07 13:05:22 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,16 +1,16 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Mac.html", "text": "Mac"},
+    {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "FDroid.html", "text": "FDroid"},
-    {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "Shell.html", "text": "Shell"},
     {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "git.html", "text": "git"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
     {"link": "MacBookAir.html", "text": "MacBookAir"},
     {"link": "OilShell.html", "text": "OilShell"},
-    {"link": "Mac.html", "text": "Mac"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88PC.html", "text": "ノートPC"},
     {"link": "Home.html", "text": "Home"},
