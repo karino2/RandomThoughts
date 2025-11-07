f2a8a0447b65b2cbee1c7643a589096d2ae20295
Kazuma Arino <hogeika2@gmail.com>
2025-11-07 12:05:21 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,13 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
+    {"link": "FDroid.html", "text": "FDroid"},
     {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "Shell.html", "text": "Shell"},
     {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "git.html", "text": "git"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
     {"link": "MacBookAir.html", "text": "MacBookAir"},
-    {"link": "FDroid.html", "text": "FDroid"},
     {"link": "OilShell.html", "text": "OilShell"},
     {"link": "Mac.html", "text": "Mac"},
     {"link": "SurfLog.html", "text": "SurfLog"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "%E3%82%AC%E3%82%B9%E3%83%95%E3%82%A1%E3%83%B3%E3%83%92%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "ガスファンヒーター"},
-    {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
    ]
 }
 ]
