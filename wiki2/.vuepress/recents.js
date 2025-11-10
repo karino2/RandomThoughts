3e4231546130f89a03efe3f4a8eec9c4d2affcb9
Kazuma Arino <hogeika2@gmail.com>
2025-11-10 16:05:19 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%912D%E3%82%B0%E3%83%A9%E3%83%95%E3%82%A3%E3%83%83%E3%82%AF%E3%82%B9%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "【書籍】2Dグラフィックスのしくみ"},
     {"link": "CG.html", "text": "CG"},
@@ -13,7 +14,6 @@ export const recents = [
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
     {"link": "MacBookAir.html", "text": "MacBookAir"},
     {"link": "OilShell.html", "text": "OilShell"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88PC.html", "text": "ノートPC"},
     {"link": "Home.html", "text": "Home"},
    ]
