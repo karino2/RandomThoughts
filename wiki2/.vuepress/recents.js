1ec4cb00a055def93216f66c0161d391b661b40b
Kazuma Arino <hogeika2@gmail.com>
2025-11-10 15:05:19 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%912D%E3%82%B0%E3%83%A9%E3%83%95%E3%82%A3%E3%83%83%E3%82%AF%E3%82%B9%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "【書籍】2Dグラフィックスのしくみ"},
     {"link": "CG.html", "text": "CG"},
     {"link": "Mac.html", "text": "Mac"},
-    {"link": "2025%E5%B9%B411%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年11月上旬の雑記"},
     {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "FDroid.html", "text": "FDroid"},
     {"link": "Shell.html", "text": "Shell"},
