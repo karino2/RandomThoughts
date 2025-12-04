9b41bfc1139a85a64819da738a7fa2a3321081c2
Kazuma Arino <hogeika2@gmail.com>
2025-12-04 19:05:28 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "PowerShell.html", "text": "PowerShell"},
     {"link": "2025%E5%B9%B412%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年12月上旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
@@ -8,7 +9,6 @@ export const recents = [
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
     {"link": "vcpkg%E3%81%A7Qt5.html", "text": "vcpkgでQt5"},
-    {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "MacBookAir.html", "text": "MacBookAir"},
     {"link": "vcpkg.html", "text": "vcpkg"},
     {"link": "Qt.html", "text": "Qt"},
