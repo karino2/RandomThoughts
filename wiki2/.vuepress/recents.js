722b479ebc66712a04a3fcbd3daa3d4df643f137
Kazuma Arino <hogeika2@gmail.com>
2025-12-12 20:05:20 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,13 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
+    {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
+    {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
+    {"link": "2025%E5%B9%B412%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年12月中旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%A9%E3%82%A4%E3%83%95.html", "text": "プログラマライフ"},
     {"link": "Home.html", "text": "Home"},
-    {"link": "2025%E5%B9%B412%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年12月中旬の雑記"},
     {"link": "%E7%9D%80%E3%82%8B%E6%AF%9B%E5%B8%83.html", "text": "着る毛布"},
     {"link": "USB%E5%85%85%E9%9B%BB%E5%99%A8.html", "text": "USB充電器"},
     {"link": "PSDotenvrc.html", "text": "PSDotenvrc"},
@@ -13,9 +16,6 @@ export const recents = [
     {"link": "VisualStudio.html", "text": "VisualStudio"},
     {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
     {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
-    {"link": "vcpkg%E3%81%A7Qt5.html", "text": "vcpkgでQt5"},
-    {"link": "MacBookAir.html", "text": "MacBookAir"},
-    {"link": "vcpkg.html", "text": "vcpkg"},
    ]
 }
 ]
