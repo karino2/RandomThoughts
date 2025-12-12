960deb91c4ec02128fe74412038d9f6b2d961712
Kazuma Arino <hogeika2@gmail.com>
2025-12-12 21:05:22 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
+    {"link": "vcpkg.html", "text": "vcpkg"},
+    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "2025%E5%B9%B412%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年12月中旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
     {"link": "VisualStudio.html", "text": "VisualStudio"},
     {"link": "%E3%83%91%E3%82%AA%E7%A2%81.html", "text": "パオ碁"},
-    {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
    ]
 }
 ]
