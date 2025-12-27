1310e80b2592d9a99d11bd9befcb0cd3d4e20117
Kazuma Arino <hogeika2@gmail.com>
2025-12-27 18:05:26 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
+    {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2025%E5%B9%B412%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年12月下旬の雑記"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%87%AA%E7%94%B1%E3%81%AE%E5%91%BD%E9%81%8B.html", "text": "【書籍】自由の命運"},
     {"link": "%E7%B5%8C%E6%B8%88.html", "text": "経済"},
     {"link": "Home.html", "text": "Home"},
@@ -12,10 +14,8 @@ export const recents = [
     {"link": "Mac.html", "text": "Mac"},
     {"link": "PowerShell.html", "text": "PowerShell"},
     {"link": "Shell.html", "text": "Shell"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
-    {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
    ]
 }
 ]
