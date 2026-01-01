ab8e46a911b18b391078fd9d00fd003511eacf41
Kazuma Arino <hogeika2@gmail.com>
2026-01-01 15:05:21 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,15 +1,15 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Home.html", "text": "Home"},
+    {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月上旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "2025%E5%B9%B412%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2025年12月下旬の雑記"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
     {"link": "%E3%83%AF%E3%83%BC%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "ワーケーション"},
     {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%87%AA%E7%94%B1%E3%81%AE%E5%91%BD%E9%81%8B.html", "text": "【書籍】自由の命運"},
     {"link": "%E7%B5%8C%E6%B8%88.html", "text": "経済"},
-    {"link": "Home.html", "text": "Home"},
     {"link": "vcpkg.html", "text": "vcpkg"},
     {"link": "MFG.html", "text": "MFG"},
     {"link": "Mac.html", "text": "Mac"},
