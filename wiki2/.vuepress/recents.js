e118b634580f14530e132f57cfa9257d3efa3960
Kazuma Arino <hogeika2@gmail.com>
2026-01-04 14:05:23 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,14 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "slog.html", "text": "slog"},
+    {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月上旬の雑記"},
     {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "Wiki%E3%81%A8%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "Wikiとノート"},
     {"link": "Wiki%E3%81%A8howm%E3%81%A8%E3%83%A1%E3%83%A2%E3%83%84%E3%83%BC%E3%83%AB.html", "text": "Wikiとhowmとメモツール"},
     {"link": "Syncthing.html", "text": "Syncthing"},
-    {"link": "slog.html", "text": "slog"},
-    {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月上旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "%E8%8B%B1%E8%AA%9E.html", "text": "英語"},
     {"link": "%E9%98%B2%E5%AF%92%E3%82%B0%E3%83%83%E3%82%BA.html", "text": "防寒グッズ"},
