ad01c276ca5b53370fb1c5ab757f69b184860bec
Kazuma Arino <hogeika2@gmail.com>
2026-01-04 13:05:21 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,21 +1,21 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
+    {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
+    {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
+    {"link": "Wiki%E3%81%A8%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "Wikiとノート"},
+    {"link": "Wiki%E3%81%A8howm%E3%81%A8%E3%83%A1%E3%83%A2%E3%83%84%E3%83%BC%E3%83%AB.html", "text": "Wikiとhowmとメモツール"},
+    {"link": "Syncthing.html", "text": "Syncthing"},
+    {"link": "slog.html", "text": "slog"},
+    {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月上旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "%E8%8B%B1%E8%AA%9E.html", "text": "英語"},
     {"link": "%E9%98%B2%E5%AF%92%E3%82%B0%E3%83%83%E3%82%BA.html", "text": "防寒グッズ"},
-    {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月上旬の雑記"},
     {"link": "%E9%9B%BB%E5%AD%90%E3%83%AC%E3%83%B3%E3%82%B8%E3%81%82%E3%82%93%E3%81%8B.html", "text": "電子レンジあんか"},
     {"link": "%E7%9D%80%E3%82%8B%E6%AF%9B%E5%B8%83.html", "text": "着る毛布"},
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
     {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "Home.html", "text": "Home"},
-    {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
-    {"link": "%E3%83%AF%E3%83%BC%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "ワーケーション"},
-    {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%9B%BD%E5%AE%B6%E3%81%AF%E3%81%AA%E3%81%9C%E8%A1%B0%E9%80%80%E3%81%99%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】国家はなぜ衰退するのか"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%87%AA%E7%94%B1%E3%81%AE%E5%91%BD%E9%81%8B.html", "text": "【書籍】自由の命運"},
-    {"link": "%E7%B5%8C%E6%B8%88.html", "text": "経済"},
    ]
 }
 ]
