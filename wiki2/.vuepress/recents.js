2016b7a325aceabb64e53c11070b235e73a1f2aa
Kazuma Arino <hogeika2@gmail.com>
2026-01-05 12:05:22 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月上旬の雑記"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "slog.html", "text": "slog"},
     {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
