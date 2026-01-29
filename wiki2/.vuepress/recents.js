3bdaaa4c67d9cfc17cb4c545b63962317d933cc0
Kazuma Arino <hogeika2@gmail.com>
2026-01-29 23:05:28 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%82%A3%E8%80%85%E3%81%AE%E8%A9%B1%E3%81%AF%E5%8C%BB%E5%B8%AB%E3%81%AB%E3%81%A9%E3%81%86%E8%81%9E%E3%81%93%E3%81%88%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】患者の話は医師にどう聞こえるのか"},
     {"link": "CG.html", "text": "CG"},
     {"link": "2026%E5%B9%B41%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年1月下旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%82%A3%E8%80%85%E3%81%AE%E8%A9%B1%E3%81%AF%E5%8C%BB%E5%B8%AB%E3%81%AB%E3%81%A9%E3%81%86%E8%81%9E%E3%81%93%E3%81%88%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】患者の話は医師にどう聞こえるのか"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "slog.html", "text": "slog"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
