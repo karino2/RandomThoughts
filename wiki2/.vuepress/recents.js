85b0df4011c46a2eabd2a628b10f11f238f3ff1a
Kazuma Arino <hogeika2@gmail.com>
2026-02-06 19:17:35 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "slog.html", "text": "slog"},
     {"link": "zx.html", "text": "zx"},
     {"link": "Shell.html", "text": "Shell"},
@@ -10,7 +11,6 @@ export const recents = [
     {"link": "Home.html", "text": "Home"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E5%8C%BB%E5%AD%A6.html", "text": "医学"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%82%A3%E8%80%85%E3%81%AE%E8%A9%B1%E3%81%AF%E5%8C%BB%E5%B8%AB%E3%81%AB%E3%81%A9%E3%81%86%E8%81%9E%E3%81%93%E3%81%88%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】患者の話は医師にどう聞こえるのか"},
     {"link": "CG.html", "text": "CG"},
     {"link": "%E9%9B%BB%E5%AD%90%E3%83%AC%E3%83%B3%E3%82%B8%E3%81%82%E3%82%93%E3%81%8B.html", "text": "電子レンジあんか"},
