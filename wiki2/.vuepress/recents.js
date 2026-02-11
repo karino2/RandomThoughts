75576683ce19f4538d0825725ec43cc317eb1716
Kazuma Arino <hogeika2@gmail.com>
2026-02-11 17:05:25 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "Home.html", "text": "Home"},
-    {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月中旬の雑記"},
-    {"link": "MFG.html", "text": "MFG"},
+    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月中旬の雑記"},
+    {"link": "Home.html", "text": "Home"},
+    {"link": "MFG.html", "text": "MFG"},
     {"link": "slog.html", "text": "slog"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%82%A3%E8%80%85%E3%81%AE%E8%A9%B1%E3%81%AF%E5%8C%BB%E5%B8%AB%E3%81%AB%E3%81%A9%E3%81%86%E8%81%9E%E3%81%93%E3%81%88%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】患者の話は医師にどう聞こえるのか"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E5%8C%BB%E5%AD%A6.html", "text": "医学"},
     {"link": "CG.html", "text": "CG"},
     {"link": "%E9%9B%BB%E5%AD%90%E3%83%AC%E3%83%B3%E3%82%B8%E3%81%82%E3%82%93%E3%81%8B.html", "text": "電子レンジあんか"},
-    {"link": "%E4%BD%9C%E6%A5%AD%E3%83%AD%E3%82%B0.html", "text": "作業ログ"},
    ]
 }
 ]
