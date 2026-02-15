88b9c50b292b08b154bae316c86e5d8a47e6f2fa
Kazuma Arino <hogeika2@gmail.com>
2026-02-15 12:34:39 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "unimemo.html", "text": "unimemo"},
     {"link": "ashx.html", "text": "ashx"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月中旬の雑記"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
     {"link": "zx.html", "text": "zx"},
     {"link": "Shell.html", "text": "Shell"},
-    {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
    ]
 }
 ]
