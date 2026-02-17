cdac6c3946c04c2e66eb5119bd45f91da31bea8c
Kazuma Arino <hogeika2@gmail.com>
2026-02-17 14:06:51 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
+    {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
     {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月中旬の雑記"},
     {"link": "%E5%B0%8F%E7%B2%8B%E3%81%AA%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88%E8%A8%80%E8%AA%9E%E3%81%8C%E6%AC%B2%E3%81%97%E3%81%84.html", "text": "小粋なスクリプト言語が欲しい"},
     {"link": "SurfLog.html", "text": "SurfLog"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "Starlark.html", "text": "Starlark"},
     {"link": "MFG.html", "text": "MFG"},
     {"link": "slog.html", "text": "slog"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%8B%B1%E4%BC%9A%E8%A9%B1%E3%83%95%E3%83%AC%E3%83%BC%E3%82%BA%E5%A4%A7%E7%89%B9%E8%A8%93.html", "text": "【書籍】英会話フレーズ大特訓"},
-    {"link": "zx.html", "text": "zx"},
    ]
 }
 ]
