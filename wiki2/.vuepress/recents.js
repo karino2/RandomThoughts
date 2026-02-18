76b344e9e3eb6ae1e330de96dd579c5d53286d4c
Kazuma Arino <hogeika2@gmail.com>
2026-02-18 12:18:46 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,13 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "SurfLog_202504%E3%81%8B%E3%82%89202512%E3%81%BE%E3%81%A7.html", "text": "SurfLog_202504から202512まで"},
+    {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月中旬の雑記"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
-    {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月中旬の雑記"},
     {"link": "%E5%B0%8F%E7%B2%8B%E3%81%AA%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88%E8%A8%80%E8%AA%9E%E3%81%8C%E6%AC%B2%E3%81%97%E3%81%84.html", "text": "小粋なスクリプト言語が欲しい"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "unimemo.html", "text": "unimemo"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "Home.html", "text": "Home"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheChecklistManifesto.html", "text": "【書籍】TheChecklistManifesto"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%82%A3%E8%80%85%E3%81%AE%E8%A9%B1%E3%81%AF%E5%8C%BB%E5%B8%AB%E3%81%AB%E3%81%A9%E3%81%86%E8%81%9E%E3%81%93%E3%81%88%E3%82%8B%E3%81%AE%E3%81%8B.html", "text": "【書籍】患者の話は医師にどう聞こえるのか"},
     {"link": "Starlark.html", "text": "Starlark"},
-    {"link": "MFG.html", "text": "MFG"},
-    {"link": "slog.html", "text": "slog"},
    ]
 }
 ]
