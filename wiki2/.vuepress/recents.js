e0aecbb0e4c4dd4d9c72ef30e67e31d784f5d9a6
Kazuma Arino <hogeika2@gmail.com>
2026-02-22 15:05:31 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,14 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "Cpp.html", "text": "Cpp"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月下旬の雑記"},
+    {"link": "Cpp.html", "text": "Cpp"},
     {"link": "Home.html", "text": "Home"},
     {"link": "Rhino.html", "text": "Rhino"},
     {"link": "JavaScript.html", "text": "JavaScript"},
     {"link": "EmbeddedLang.html", "text": "EmbeddedLang"},
     {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "SurfLog_202504%E3%81%8B%E3%82%89202512%E3%81%BE%E3%81%A7.html", "text": "SurfLog_202504から202512まで"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
