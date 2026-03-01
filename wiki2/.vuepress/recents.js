3f9510c3fa1f12061e753ea778ca4f2091e16ff3
Kazuma Arino <hogeika2@gmail.com>
2026-03-01 18:05:33 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -5,7 +5,6 @@ export const recents = [
     {"link": "%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E9%9B%91%E8%AB%87.html", "text": "プログラム雑談"},
     {"link": "Home.html", "text": "Home"},
     {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月下旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%87%AA%E7%94%B1%E3%81%AE%E5%91%BD%E9%81%8B.html", "text": "【書籍】自由の命運"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheChecklistManifesto.html", "text": "【書籍】TheChecklistManifesto"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
@@ -16,6 +15,7 @@ export const recents = [
     {"link": "Rhino.html", "text": "Rhino"},
     {"link": "JavaScript.html", "text": "JavaScript"},
     {"link": "EmbeddedLang.html", "text": "EmbeddedLang"},
+    {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
    ]
 }
 ]
