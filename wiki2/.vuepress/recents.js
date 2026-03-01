5c1e7ab2bdadccbbcb53b14c64aca991eadaaf7e
Kazuma Arino <hogeika2@gmail.com>
2026-03-01 12:05:29 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Home.html", "text": "Home"},
+    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月上旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B42%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年2月下旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%87%AA%E7%94%B1%E3%81%AE%E5%91%BD%E9%81%8B.html", "text": "【書籍】自由の命運"},
@@ -10,12 +12,10 @@ export const recents = [
     {"link": "%E5%B0%8F%E7%B2%8B%E3%81%AA%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88%E8%A8%80%E8%AA%9E%E3%81%8C%E6%AC%B2%E3%81%97%E3%81%84.html", "text": "小粋なスクリプト言語が欲しい"},
     {"link": "ashx.html", "text": "ashx"},
     {"link": "Cpp.html", "text": "Cpp"},
-    {"link": "Home.html", "text": "Home"},
     {"link": "Rhino.html", "text": "Rhino"},
     {"link": "JavaScript.html", "text": "JavaScript"},
     {"link": "EmbeddedLang.html", "text": "EmbeddedLang"},
     {"link": "SurfLog%E3%81%AE%E9%81%8E%E5%8E%BB%E3%83%AD%E3%82%B0.html", "text": "SurfLogの過去ログ"},
-    {"link": "SurfLog_202504%E3%81%8B%E3%82%89202512%E3%81%BE%E3%81%A7.html", "text": "SurfLog_202504から202512まで"},
    ]
 }
 ]
