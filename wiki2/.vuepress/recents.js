ecd5e91d50352fe48870877883431229a21dbfd8
Kazuma Arino <hogeika2@gmail.com>
2026-03-02 21:05:31 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,12 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E7%94%9F%E7%90%86%E5%AD%A6.html", "text": "生理学"},
+    {"link": "%E7%94%9F%E7%89%A9%E5%AD%A6.html", "text": "生物学"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%96%B0%E3%81%97%E3%81%84%E4%BA%BA%E4%BD%93%E3%81%AE%E6%95%99%E7%A7%91%E6%9B%B8.html", "text": "【書籍】新しい人体の教科書"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%AE%E3%83%A3%E3%83%8E%E3%83%B3%E3%82%B0%E7%94%9F%E7%90%86%E5%AD%A6.html", "text": "【書籍】ギャノング生理学"},
+    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
+    {"link": "%E5%8C%BB%E5%AD%A6.html", "text": "医学"},
     {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月上旬の雑記"},
     {"link": "%E3%82%B9%E3%83%88%E3%83%AC%E3%83%83%E3%83%81.html", "text": "ストレッチ"},
     {"link": "%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E9%9B%91%E8%AB%87.html", "text": "プログラム雑談"},
@@ -10,12 +16,6 @@ export const recents = [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheChecklistManifesto.html", "text": "【書籍】TheChecklistManifesto"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
-    {"link": "%E5%B0%8F%E7%B2%8B%E3%81%AA%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88%E8%A8%80%E8%AA%9E%E3%81%8C%E6%AC%B2%E3%81%97%E3%81%84.html", "text": "小粋なスクリプト言語が欲しい"},
-    {"link": "ashx.html", "text": "ashx"},
-    {"link": "Cpp.html", "text": "Cpp"},
-    {"link": "Rhino.html", "text": "Rhino"},
-    {"link": "JavaScript.html", "text": "JavaScript"},
-    {"link": "EmbeddedLang.html", "text": "EmbeddedLang"},
    ]
 }
 ]
