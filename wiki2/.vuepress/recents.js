35ca4335244a4c8414a40700650bf53728b5cb5e
Kazuma Arino <hogeika2@gmail.com>
2026-03-06 12:28:38 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%83%8E%E3%83%BC%E3%83%88PC.html", "text": "ノートPC"},
+    {"link": "%E3%82%BF%E3%83%96%E3%83%AC%E3%83%83%E3%83%88.html", "text": "タブレット"},
+    {"link": "LenovoTabP12.html", "text": "LenovoTabP12"},
     {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月上旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%AE%E3%83%A3%E3%83%8E%E3%83%B3%E3%82%B0%E7%94%9F%E7%90%86%E5%AD%A6.html", "text": "【書籍】ギャノング生理学"},
     {"link": "%E7%94%9F%E7%90%86%E5%AD%A6.html", "text": "生理学"},
@@ -13,9 +16,6 @@ export const recents = [
     {"link": "%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E9%9B%91%E8%AB%87.html", "text": "プログラム雑談"},
     {"link": "Home.html", "text": "Home"},
     {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%87%AA%E7%94%B1%E3%81%AE%E5%91%BD%E9%81%8B.html", "text": "【書籍】自由の命運"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheChecklistManifesto.html", "text": "【書籍】TheChecklistManifesto"},
-    {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
    ]
 }
 ]
