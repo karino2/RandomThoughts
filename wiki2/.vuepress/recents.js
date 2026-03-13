633ea50a9d14ba5a05e7cd8f4cc4c6bda88abe50
Kazuma Arino <hogeika2@gmail.com>
2026-03-14 00:05:30 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "ashx.html", "text": "ashx"},
     {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月中旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "Home.html", "text": "Home"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%82%BF%E3%83%96%E3%83%AC%E3%83%83%E3%83%88.html", "text": "タブレット"},
     {"link": "LenovoTabP12.html", "text": "LenovoTabP12"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%AE%E3%83%A3%E3%83%8E%E3%83%B3%E3%82%B0%E7%94%9F%E7%90%86%E5%AD%A6.html", "text": "【書籍】ギャノング生理学"},
-    {"link": "%E7%94%9F%E7%90%86%E5%AD%A6.html", "text": "生理学"},
    ]
 }
 ]
