da7d1460d3cb8cbe700e6e951a5f1d8d87448933
Kazuma Arino <hogeika2@gmail.com>
2026-03-20 21:05:31 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月中旬の雑記"},
     {"link": "%E3%82%A4%E3%83%A4%E3%83%BC%E3%83%9E%E3%83%95.html", "text": "イヤーマフ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "git.html", "text": "git"},
-    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月中旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%BD%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E3%83%97%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "【書籍】ソーシャルメディアプリズム"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
     {"link": "%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%8D%E3%83%83%E3%83%88%E3%83%AA%E3%83%86%E3%83%A9%E3%82%B7%E3%83%BC.html", "text": "インターネットリテラシー"},
