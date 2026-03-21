5ebfd8b852f64ecc5855f6ae84cf821809a890de
Kazuma Arino <hogeika2@gmail.com>
2026-03-21 10:05:35 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Home.html", "text": "Home"},
+    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月下旬の雑記"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
-    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月中旬の雑記"},
     {"link": "%E3%82%A4%E3%83%A4%E3%83%BC%E3%83%9E%E3%83%95.html", "text": "イヤーマフ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "git.html", "text": "git"},
@@ -11,7 +12,6 @@ export const recents = [
     {"link": "%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%8D%E3%83%83%E3%83%88%E3%83%AA%E3%83%86%E3%83%A9%E3%82%B7%E3%83%BC.html", "text": "インターネットリテラシー"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheChecklistManifesto.html", "text": "【書籍】TheChecklistManifesto"},
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
-    {"link": "Home.html", "text": "Home"},
     {"link": "Rhino.html", "text": "Rhino"},
     {"link": "kanau.html", "text": "kanau"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
