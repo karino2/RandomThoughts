40c5bec026af2d4f8aefa8f4bd4fdddfc0dd94ea
Kazuma Arino <hogeika2@gmail.com>
2026-03-26 23:05:31 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,13 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
+    {"link": "Starlarcs.html", "text": "Starlarcs"},
     {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月下旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E5%86%99%E7%9C%9Fmd%E3%83%A1%E3%83%A2.html", "text": "写真mdメモ"},
     {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%BD%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E3%83%97%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "【書籍】ソーシャルメディアプリズム"},
-    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "%E3%82%A4%E3%83%A4%E3%83%BC%E3%83%9E%E3%83%95.html", "text": "イヤーマフ"},
     {"link": "git.html", "text": "git"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheChecklistManifesto.html", "text": "【書籍】TheChecklistManifesto"},
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
     {"link": "Rhino.html", "text": "Rhino"},
-    {"link": "kanau.html", "text": "kanau"},
    ]
 }
 ]
