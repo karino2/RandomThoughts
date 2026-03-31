ea7477b0a703d578773c4f520b3310d767a894f2
Kazuma Arino <hogeika2@gmail.com>
2026-04-01 00:05:40 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月下旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E4%BA%BA%E6%9C%88%E3%81%AE%E7%A5%9E%E8%A9%B1.html", "text": "【書籍】人月の神話"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "2026%E5%B9%B43%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年3月下旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%9C%80%E5%BC%B7%E3%81%AE%E9%9B%86%E4%B8%AD%E5%8A%9B.html", "text": "【書籍】最強の集中力"},
     {"link": "%E5%86%99%E7%9C%9Fmd%E3%83%A1%E3%83%A2.html", "text": "写真mdメモ"},
     {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
