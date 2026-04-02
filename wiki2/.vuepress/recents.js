41074238b9fbd99de6d375c06bbbe3984a2c2bf1
Kazuma Arino <hogeika2@gmail.com>
2026-04-02 23:05:36 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "2026%E5%B9%B44%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年4月上旬の雑記"},
-    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E4%BD%9C%E3%82%8A%E3%81%9F%E3%81%84%E3%81%A8%E6%80%9D%E3%81%A3%E3%81%A6%E3%81%84%E3%82%8B%E4%BA%8B%E3%81%AF%E3%81%99%E3%81%90%E4%BD%9C%E3%81%A3%E3%81%9F%E6%96%B9%E3%81%8C%E3%81%84%E3%81%84.html", "text": "そのうち作りたいと思っている事はすぐ作った方がいい"},
     {"link": "Home.html", "text": "Home"},
+    {"link": "2026%E5%B9%B44%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年4月上旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%9C%80%E5%BC%B7%E3%81%AE%E9%9B%86%E4%B8%AD%E5%8A%9B.html", "text": "【書籍】最強の集中力"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
     {"link": "SurfLog.html", "text": "SurfLog"},
@@ -16,6 +15,7 @@ export const recents = [
     {"link": "SAFToDo.html", "text": "SAFToDo"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%BD%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E3%83%97%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "【書籍】ソーシャルメディアプリズム"},
+    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
    ]
 }
 ]
