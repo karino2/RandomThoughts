064b58415bacf1762e65524b78148c8d0a45e1ca
Kazuma Arino <hogeika2@gmail.com>
2026-04-08 16:18:33 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B44%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年4月上旬の雑記"},
     {"link": "%E5%86%99%E7%9C%9Fmd%E3%83%A1%E3%83%A2.html", "text": "写真mdメモ"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E4%BA%BA%E6%9C%88%E3%81%AE%E7%A5%9E%E8%A9%B1.html", "text": "【書籍】人月の神話"},
     {"link": "BOOX.html", "text": "BOOX"},
     {"link": "%E3%82%BF%E3%83%96%E3%83%AC%E3%83%83%E3%83%88.html", "text": "タブレット"},
