fa221db3b0651e76c4cb26b8e9b0087c09b9e957
Kazuma Arino <hogeika2@gmail.com>
2026-04-10 19:05:34 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%82%AE%E3%82%BF%E3%83%BC.html", "text": "ギター"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B44%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年4月上旬の雑記"},
     {"link": "%E3%82%B7%E3%83%A3%E3%83%AF%E3%83%BC%E3%83%98%E3%83%83%E3%83%89.html", "text": "シャワーヘッド"},
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E5%86%99%E7%9C%9Fmd%E3%83%A1%E3%83%A2.html", "text": "写真mdメモ"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E4%BA%BA%E6%9C%88%E3%81%AE%E7%A5%9E%E8%A9%B1.html", "text": "【書籍】人月の神話"},
     {"link": "BOOX.html", "text": "BOOX"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "SAFToDo.html", "text": "SAFToDo"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E5%A5%84%E7%BE%8E%E5%A4%A7%E5%B3%B6%E8%A8%88%E7%94%BB.html", "text": "奄美大島計画"},
-    {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
    ]
 }
 ]
