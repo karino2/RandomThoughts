ab43b88b849a3de5970e36e91fc5d1bc29e55877
Kazuma Arino <hogeika2@gmail.com>
2026-04-30 13:05:43 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,13 +1,13 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Shell.html", "text": "Shell"},
     {"link": "2026%E5%B9%B44%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年4月下旬の雑記"},
     {"link": "%E9%9B%BB%E5%8B%95%E3%82%A2%E3%82%B7%E3%82%B9%E3%83%88%E8%87%AA%E8%BB%A2%E8%BB%8A.html", "text": "電動アシスト自転車"},
     {"link": "Dio110.html", "text": "Dio110"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "Windows.html", "text": "Windows"},
     {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
-    {"link": "Shell.html", "text": "Shell"},
     {"link": "VSCode.html", "text": "VSCode"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E4%BA%BA%E6%9C%88%E3%81%AE%E7%A5%9E%E8%A9%B1.html", "text": "【書籍】人月の神話"},
