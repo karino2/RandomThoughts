fce532bb6eb0bcba84bb8ab9c92af80f8a49ec97
Kazuma Arino <hogeika2@gmail.com>
2026-05-04 10:05:50 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月上旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E9%85%8D%E9%A3%9F%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9.html", "text": "配食サービス"},
     {"link": "%E9%A3%9F%E4%BA%8B%E9%96%A2%E9%80%A3.html", "text": "食事関連"},
-    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月上旬の雑記"},
     {"link": "Home.html", "text": "Home"},
     {"link": "Shell.html", "text": "Shell"},
     {"link": "%E9%9B%BB%E5%8B%95%E3%82%A2%E3%82%B7%E3%82%B9%E3%83%88%E8%87%AA%E8%BB%A2%E8%BB%8A.html", "text": "電動アシスト自転車"},
