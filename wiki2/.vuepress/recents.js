2dee354a76d7589be0ff7a3479bebaf721fc9639
Kazuma Arino <hogeika2@gmail.com>
2026-05-09 22:05:39 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
+    {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D.html", "text": "お絵描き"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月上旬の雑記"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%82%AA%E3%83%BC%E3%83%88%E3%83%9F%E3%83%BC%E3%83%AB.html", "text": "オートミール"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
     {"link": "VSCode.html", "text": "VSCode"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E4%BA%BA%E6%9C%88%E3%81%AE%E7%A5%9E%E8%A9%B1.html", "text": "【書籍】人月の神話"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E6%9C%80%E5%BC%B7%E3%81%AE%E9%9B%86%E4%B8%AD%E5%8A%9B.html", "text": "【書籍】最強の集中力"},
-    {"link": "%E3%82%B7%E3%83%A3%E3%83%AF%E3%83%BC%E3%83%98%E3%83%83%E3%83%89.html", "text": "シャワーヘッド"},
    ]
 }
 ]
