e384bdc5663b3d67f8cf8deecb825b683b79caa2
Kazuma Arino <hogeika2@gmail.com>
2026-05-10 23:05:39 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E8%85%95%E6%99%82%E8%A8%88.html", "text": "腕時計"},
+    {"link": "BOOX.html", "text": "BOOX"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月上旬の雑記"},
+    {"link": "%E8%85%95%E6%99%82%E8%A8%88.html", "text": "腕時計"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
     {"link": "Wails.html", "text": "Wails"},
     {"link": "MdImgr.html", "text": "MdImgr"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%82%AA%E3%83%BC%E3%83%88%E3%83%9F%E3%83%BC%E3%83%AB.html", "text": "オートミール"},
     {"link": "%E9%85%8D%E9%A3%9F%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9.html", "text": "配食サービス"},
     {"link": "%E9%A3%9F%E4%BA%8B%E9%96%A2%E9%80%A3.html", "text": "食事関連"},
-    {"link": "Home.html", "text": "Home"},
    ]
 }
 ]
