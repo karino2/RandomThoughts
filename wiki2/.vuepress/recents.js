810b32c08cd79dcd20686a991f891d367c3fb383
Kazuma Arino <hogeika2@gmail.com>
2026-05-11 21:05:40 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E9%85%8D%E9%A3%9F%E3%81%AE%E3%81%B5%E3%82%8C%E6%84%9B.html", "text": "配食のふれ愛"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D.html", "text": "お絵描き"},
     {"link": "%E3%82%AA%E3%83%BC%E3%83%88%E3%83%9F%E3%83%BC%E3%83%AB.html", "text": "オートミール"},
-    {"link": "%E9%85%8D%E9%A3%9F%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9.html", "text": "配食サービス"},
    ]
 }
 ]
