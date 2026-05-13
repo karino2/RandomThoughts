4a0ead3fc4c09ebb5cef3c873cf63db97c759910
Kazuma Arino <hogeika2@gmail.com>
2026-05-13 18:05:45 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "BOOX.sync-conflict-20260513-171914-WI4LIFX.html", "text": "BOOX.sync-conflict-20260513-171914-WI4LIFX"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "Kotlin%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9%E3%81%AE%E5%92%8C%E8%A8%B3.html", "text": "Kotlinリファレンスの和訳"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E8%85%95%E6%99%82%E8%A8%88.html", "text": "腕時計"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
-    {"link": "Wails.html", "text": "Wails"},
    ]
 }
 ]
