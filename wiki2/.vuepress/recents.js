99a6c5f56dbeca5b14d7974d71266ae75bf6ee7b
Kazuma Arino <hogeika2@gmail.com>
2026-05-13 20:05:43 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "StorageAccessFramework.html", "text": "StorageAccessFramework"},
     {"link": "BOOX.sync-conflict-20260513-171914-WI4LIFX.html", "text": "BOOX.sync-conflict-20260513-171914-WI4LIFX"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
-    {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "Kotlin%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9%E3%81%AE%E5%92%8C%E8%A8%B3.html", "text": "Kotlinリファレンスの和訳"},
     {"link": "Kotlin.html", "text": "Kotlin"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
