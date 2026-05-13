219f0e5b82ef6cf3362a1e482370c65818c2b7de
Kazuma Arino <hogeika2@gmail.com>
2026-05-13 22:05:41 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Rhino.html", "text": "Rhino"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "StorageAccessFramework.html", "text": "StorageAccessFramework"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%82%AC%E3%82%B9%E3%83%95%E3%82%A1%E3%83%B3%E3%83%92%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "ガスファンヒーター"},
     {"link": "%E9%85%8D%E9%A3%9F%E3%81%AE%E3%81%B5%E3%82%8C%E6%84%9B.html", "text": "配食のふれ愛"},
     {"link": "Home.html", "text": "Home"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
    ]
 }
 ]
