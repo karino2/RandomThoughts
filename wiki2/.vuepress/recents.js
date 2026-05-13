8a98b5f35579d5c8de42cdee89785bc87a7e00bd
Kazuma Arino <hogeika2@gmail.com>
2026-05-13 11:05:43 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,21 +1,21 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
+    {"link": "Starlarcs.html", "text": "Starlarcs"},
+    {"link": "Rhinocs.html", "text": "Rhinocs"},
+    {"link": "BOOX.html", "text": "BOOX"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%82%AC%E3%82%B9%E3%83%95%E3%82%A1%E3%83%B3%E3%83%92%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "ガスファンヒーター"},
     {"link": "%E9%85%8D%E9%A3%9F%E3%81%AE%E3%81%B5%E3%82%8C%E6%84%9B.html", "text": "配食のふれ愛"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
-    {"link": "BOOX.html", "text": "BOOX"},
     {"link": "%E8%85%95%E6%99%82%E8%A8%88.html", "text": "腕時計"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
     {"link": "Wails.html", "text": "Wails"},
     {"link": "MdImgr.html", "text": "MdImgr"},
     {"link": "%E4%B8%96%E7%95%8C%E5%8F%B2.html", "text": "世界史"},
-    {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
-    {"link": "Dio110.html", "text": "Dio110"},
-    {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D.html", "text": "お絵描き"},
    ]
 }
 ]
