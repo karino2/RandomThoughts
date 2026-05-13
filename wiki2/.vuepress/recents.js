75f5cfbcdbbbe6632f832abfc2b367b133250f41
Kazuma Arino <hogeika2@gmail.com>
2026-05-13 12:05:48 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Kotlin%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9%E3%81%AE%E5%92%8C%E8%A8%B3.html", "text": "Kotlinリファレンスの和訳"},
+    {"link": "Kotlin.html", "text": "Kotlin"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "Starlarcs.html", "text": "Starlarcs"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "%E8%85%95%E6%99%82%E8%A8%88.html", "text": "腕時計"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
     {"link": "Wails.html", "text": "Wails"},
-    {"link": "MdImgr.html", "text": "MdImgr"},
-    {"link": "%E4%B8%96%E7%95%8C%E5%8F%B2.html", "text": "世界史"},
    ]
 }
 ]
