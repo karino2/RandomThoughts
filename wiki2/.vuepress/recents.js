a1ab720f5a2fb0efce8244d6847240fff49b8bf9
Kazuma Arino <hogeika2@gmail.com>
2026-05-14 21:05:44 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
-    {"link": "%E3%82%A8%E3%83%87%E3%82%A3%E3%82%BF%E8%AA%BF%E6%9F%BB.html", "text": "エディタ調査"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
+    {"link": "%E3%82%A8%E3%83%87%E3%82%A3%E3%82%BF%E8%AA%BF%E6%9F%BB.html", "text": "エディタ調査"},
     {"link": "Rhino.html", "text": "Rhino"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "StorageAccessFramework.html", "text": "StorageAccessFramework"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "BOOX.html", "text": "BOOX"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%82%AC%E3%82%B9%E3%83%95%E3%82%A1%E3%83%B3%E3%83%92%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "ガスファンヒーター"},
-    {"link": "%E9%85%8D%E9%A3%9F%E3%81%AE%E3%81%B5%E3%82%8C%E6%84%9B.html", "text": "配食のふれ愛"},
    ]
 }
 ]
