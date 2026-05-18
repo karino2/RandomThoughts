daf18183ddf430e445c72e6d3a2375c063babd4a
Kazuma Arino <hogeika2@gmail.com>
2026-05-18 21:05:40 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
-    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "%E3%82%AD%E3%83%BC%E3%83%9C%E3%83%BC%E3%83%89.html", "text": "キーボード"},
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "BOOX.sync-conflict-20260513-171914-WI4LIFX.html", "text": "BOOX.sync-conflict-20260513-171914-WI4LIFX"},
     {"link": "Kotlin%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9%E3%81%AE%E5%92%8C%E8%A8%B3.html", "text": "Kotlinリファレンスの和訳"},
     {"link": "Kotlin.html", "text": "Kotlin"},
-    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
    ]
 }
 ]
