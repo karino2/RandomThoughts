139201012e26a4a6d23eda79a16f6b0719c56fc9
Kazuma Arino <hogeika2@gmail.com>
2026-05-21 00:05:43 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -2,8 +2,9 @@ export const recents = [
  {text: "Recents:",
    children: [
     {"link": "Rhinocs.html", "text": "Rhinocs"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "BOOX.html", "text": "BOOX"},
     {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月中旬の雑記"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "Syncthing.html", "text": "Syncthing"},
     {"link": "%E3%82%A8%E3%83%87%E3%82%A3%E3%82%BF%E8%AA%BF%E6%9F%BB.html", "text": "エディタ調査"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "StorageAccessFramework.html", "text": "StorageAccessFramework"},
     {"link": "BOOX.sync-conflict-20260513-171914-WI4LIFX.html", "text": "BOOX.sync-conflict-20260513-171914-WI4LIFX"},
-    {"link": "Kotlin%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9%E3%81%AE%E5%92%8C%E8%A8%B3.html", "text": "Kotlinリファレンスの和訳"},
    ]
 }
 ]
