ab80878b148b27c57321a8191a7100e4aaca75b2
Kazuma Arino <hogeika2@gmail.com>
2026-05-27 13:05:49 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "SKK.html", "text": "SKK"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "Rhinocs_%E4%BB%95%E6%A7%98%E6%A4%9C%E8%A8%8E.html", "text": "Rhinocs_仕様検討"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "Syncthing.html", "text": "Syncthing"},
     {"link": "%E3%82%AD%E3%83%BC%E3%83%9C%E3%83%BC%E3%83%89.html", "text": "キーボード"},
-    {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
    ]
 }
 ]
