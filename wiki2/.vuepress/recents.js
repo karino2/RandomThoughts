e9909fc8245cce24d99a8bf1e50cddfd6616e0d5
Kazuma Arino <hogeika2@gmail.com>
2026-05-31 13:06:02 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月下旬の雑記"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "Rhino.html", "text": "Rhino"},
-    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月下旬の雑記"},
     {"link": "SKK.html", "text": "SKK"},
     {"link": "Mac.html", "text": "Mac"},
     {"link": "Rhinocs_%E4%BB%95%E6%A7%98%E6%A4%9C%E8%A8%8E.html", "text": "Rhinocs_仕様検討"},
