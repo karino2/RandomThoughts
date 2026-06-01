b184d5b8af21b679ba83b84523cd5dd8815c0b3c
Kazuma Arino <hogeika2@gmail.com>
2026-06-01 19:05:46 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Home.html", "text": "Home"},
+    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月上旬の雑記"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "Rhino.html", "text": "Rhino"},
-    {"link": "2026%E5%B9%B45%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年5月下旬の雑記"},
     {"link": "SKK.html", "text": "SKK"},
     {"link": "Rhinocs_%E4%BB%95%E6%A7%98%E6%A4%9C%E8%A8%8E.html", "text": "Rhinocs_仕様検討"},
     {"link": "Mac.html", "text": "Mac"},
@@ -13,7 +14,6 @@ export const recents = [
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "BOOX.html", "text": "BOOX"},
     {"link": "%E3%82%A8%E3%83%87%E3%82%A3%E3%82%BF%E8%AA%BF%E6%9F%BB.html", "text": "エディタ調査"},
-    {"link": "Home.html", "text": "Home"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "Syncthing.html", "text": "Syncthing"},
    ]
