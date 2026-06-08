3812b19ae2666b8520724ff5c0a6da7c35961c74
Kazuma Arino <hogeika2@gmail.com>
2026-06-08 21:05:43 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,12 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月上旬の雑記"},
-    {"link": "BOOX.html", "text": "BOOX"},
+    {"link": "%E5%86%99%E7%9C%9Fmd%E3%83%A1%E3%83%A2.html", "text": "写真mdメモ"},
     {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
+    {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "BOOX.html", "text": "BOOX"},
+    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月上旬の雑記"},
     {"link": "HT2.html", "text": "HT2"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "Rhinocs_%E4%BB%95%E6%A7%98%E6%A4%9C%E8%A8%8E.html", "text": "Rhinocs_仕様検討"},
@@ -12,10 +15,7 @@ export const recents = [
     {"link": "%E3%82%B5%E3%83%BC%E3%83%95%E3%83%9C%E3%83%BC%E3%83%89.html", "text": "サーフボード"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "Rhino.html", "text": "Rhino"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E3%82%AA%E3%83%BC%E3%83%88%E3%83%9F%E3%83%BC%E3%83%AB.html", "text": "オートミール"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "【書籍】原論文から解き明かす生成AI"},
-    {"link": "%E6%9B%B8%E7%B1%8D.html", "text": "書籍"},
    ]
 }
 ]
