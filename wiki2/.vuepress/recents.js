71bec70b72edb4303647aecb66e5b69b3c2012e8
Kazuma Arino <hogeika2@gmail.com>
2026-06-12 15:05:47 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,21 +1,21 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E6%95%B0%E5%AD%A6.html", "text": "数学"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "【書籍】原論文から解き明かす生成AI"},
+    {"link": "%E6%9B%B8%E7%B1%8D%E3%81%AE%E8%87%AA%E5%8B%95%E7%94%9F%E6%88%90%E7%94%A8%E3%83%9A%E3%83%BC%E3%82%B8.html", "text": "書籍の自動生成用ページ"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%B5%E3%82%A4%E3%82%B3%E3%83%AD%E6%9C%AC.html", "text": "【書籍】サイコロ本"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91CoverAndThomas.html", "text": "【書籍】CoverAndThomas"},
+    {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E3%82%B5%E3%82%A4%E3%82%B3%E3%83%AD%E6%9C%AC.html", "text": "サイコロ本"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "CoverAndThomas.html", "text": "CoverAndThomas"},
     {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "BOOX.html", "text": "BOOX"},
     {"link": "PngNote.html", "text": "PngNote"},
-    {"link": "%E3%83%9E%E3%82%B0%E3%83%8A%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "マグナスケッチ"},
-    {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
-    {"link": "%E5%86%99%E7%9C%9Fmd%E3%83%A1%E3%83%A2.html", "text": "写真mdメモ"},
-    {"link": "HT2.html", "text": "HT2"},
-    {"link": "Rhinocs.html", "text": "Rhinocs"},
-    {"link": "Rhinocs_%E4%BB%95%E6%A7%98%E6%A4%9C%E8%A8%8E.html", "text": "Rhinocs_仕様検討"},
    ]
 }
 ]
