8e2201f82db6ee886b927045b7eecbfc4a401c2a
Kazuma Arino <hogeika2@gmail.com>
2026-06-13 10:05:44 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Rhinocs.html", "text": "Rhinocs"},
+    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
     {"link": "BOOX.html", "text": "BOOX"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "【書籍】原論文から解き明かす生成AI"},
-    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
     {"link": "%E6%95%B0%E5%AD%A6.html", "text": "数学"},
     {"link": "%E6%9B%B8%E7%B1%8D%E3%81%AE%E8%87%AA%E5%8B%95%E7%94%9F%E6%88%90%E7%94%A8%E3%83%9A%E3%83%BC%E3%82%B8.html", "text": "書籍の自動生成用ページ"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%B5%E3%82%A4%E3%82%B3%E3%83%AD%E6%9C%AC.html", "text": "【書籍】サイコロ本"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
-    {"link": "PngNote.html", "text": "PngNote"},
    ]
 }
 ]
