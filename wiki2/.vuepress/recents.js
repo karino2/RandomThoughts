674f4fbd1ed0a6bcac8e4f762afec0112b9e1767
Kazuma Arino <hogeika2@gmail.com>
2026-06-15 17:05:44 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Kariino2DIYApp.html", "text": "Kariino2DIYApp"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "【書籍】原論文から解き明かす生成AI"},
@@ -9,7 +10,6 @@ export const recents = [
     {"link": "SKK.html", "text": "SKK"},
     {"link": "PngNote.html", "text": "PngNote"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
-    {"link": "Kariino2DIYApp.html", "text": "Kariino2DIYApp"},
     {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
     {"link": "SAFToDo.html", "text": "SAFToDo"},
     {"link": "BOOX.html", "text": "BOOX"},
