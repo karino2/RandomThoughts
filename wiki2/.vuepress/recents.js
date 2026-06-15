3c32e1c76dcb1212c916d8bd45f5a9d46045686a
Kazuma Arino <hogeika2@gmail.com>
2026-06-16 01:36:15 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%81%B2%E3%81%96%E3%81%AE%E4%B8%8A%E3%83%86%E3%83%BC%E3%83%96%E3%83%AB.html", "text": "ひざの上テーブル"},
     {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "lazyjp.html", "text": "lazyjp"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "SKK.html", "text": "SKK"},
     {"link": "PngNote.html", "text": "PngNote"},
     {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
-    {"link": "SAFToDo.html", "text": "SAFToDo"},
    ]
 }
 ]
