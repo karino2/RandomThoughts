6cd147c65d7719821c38a0c9850d0a4fbd2a521e
Kazuma Arino <hogeika2@gmail.com>
2026-06-17 03:05:02 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,9 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
     {"link": "%E3%82%B5%E3%83%BC%E3%83%95%E3%82%A3%E3%83%B3.html", "text": "サーフィン"},
     {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
