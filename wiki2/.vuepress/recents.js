7befde12a88339c4d70b440c27f2f4f669c6f6e0
Kazuma Arino <hogeika2@gmail.com>
2026-06-18 12:05:46 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,12 +1,12 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E6%A5%BD%E5%A4%A9%E3%83%A2%E3%83%90%E3%82%A4%E3%83%AB.html", "text": "楽天モバイル"},
-    {"link": "WiMax.html", "text": "WiMax"},
     {"link": "WiFi%E3%83%AB%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "WiFiルーター"},
-    {"link": "mdvcat.html", "text": "mdvcat"},
     {"link": "Go.html", "text": "Go"},
     {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月中旬の雑記"},
+    {"link": "%E6%A5%BD%E5%A4%A9%E3%83%A2%E3%83%90%E3%82%A4%E3%83%AB.html", "text": "楽天モバイル"},
+    {"link": "WiMax.html", "text": "WiMax"},
+    {"link": "mdvcat.html", "text": "mdvcat"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E3%81%B2%E3%81%96%E3%81%AE%E4%B8%8A%E3%83%86%E3%83%BC%E3%83%96%E3%83%AB.html", "text": "ひざの上テーブル"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
