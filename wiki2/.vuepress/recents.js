2cf2eef84b7f9b4cf6b6a4f4fa36a2c139b01223
Kazuma Arino <hogeika2@gmail.com>
2026-06-25 13:05:51 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
+    {"link": "VSCode.html", "text": "VSCode"},
+    {"link": "lazyjp.html", "text": "lazyjp"},
     {"link": "2026%E5%B9%B46%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年6月下旬の雑記"},
+    {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "Dio110.html", "text": "Dio110"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "【書籍】原論文から解き明かす生成AI"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "Karino2DIYApp.html", "text": "Karino2DIYApp"},
     {"link": "%E9%9B%91%E5%A4%9A%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "雑多なメモ"},
     {"link": "%E3%82%B5%E3%83%BC%E3%83%95%E3%82%A3%E3%83%B3.html", "text": "サーフィン"},
-    {"link": "%E3%83%9E%E3%82%B0%E3%83%8A%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "マグナスケッチ"},
-    {"link": "WiFi%E3%83%AB%E3%83%BC%E3%82%BF%E3%83%BC.html", "text": "WiFiルーター"},
    ]
 }
 ]
