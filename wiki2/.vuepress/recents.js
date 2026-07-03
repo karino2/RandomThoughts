0d2d2c7ac92ea1d69e906a1100f2d101972b9c02
Kazuma Arino <hogeika2@gmail.com>
2026-07-03 19:05:49 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheArtOfComputerProgramming.html", "text": "【書籍】TheArtOfComputerProgramming"},
+    {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
+    {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E6%9C%AC.html", "text": "アルゴリズム本"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91IntroductionToAlgorithms.html", "text": "【書籍】IntroductionToAlgorithms"},
     {"link": "webviewjs.html", "text": "webviewjs"},
     {"link": "Neutralinojs.html", "text": "Neutralinojs"},
@@ -13,9 +16,6 @@ export const recents = [
     {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
     {"link": "Home.html", "text": "Home"},
     {"link": "Dio110.html", "text": "Dio110"},
-    {"link": "%E3%82%B5%E3%83%BC%E3%83%95%E3%82%A3%E3%83%B3.html", "text": "サーフィン"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "%E3%83%90%E3%82%A4%E3%82%AF.html", "text": "バイク"},
    ]
 }
 ]
