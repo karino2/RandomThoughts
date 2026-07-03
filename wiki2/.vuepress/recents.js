73f60e257776a73ff453b147f302c1ed40f6d9e1
Kazuma Arino <hogeika2@gmail.com>
2026-07-03 20:05:50 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,21 +1,21 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheArtOfComputerProgramming.html", "text": "【書籍】TheArtOfComputerProgramming"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
+    {"link": "%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92.html", "text": "機械学習"},
+    {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
+    {"link": "%E3%82%B5%E3%82%A4%E3%82%B3%E3%83%AD%E6%9C%AC.html", "text": "サイコロ本"},
+    {"link": "Wiki%E3%81%A8%E6%89%8B%E6%9B%B8%E3%81%8D%E3%83%8E%E3%83%BC%E3%83%88%E3%81%AE%E8%9E%8D%E5%90%88.html", "text": "Wikiと手書きノートの融合"},
+    {"link": "PRML.html", "text": "PRML"},
+    {"link": "HMM.html", "text": "HMM"},
+    {"link": "CoverAndThomas.html", "text": "CoverAndThomas"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheArtOfComputerProgramming.html", "text": "【書籍】TheArtOfComputerProgramming"},
     {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E6%9C%AC.html", "text": "アルゴリズム本"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91IntroductionToAlgorithms.html", "text": "【書籍】IntroductionToAlgorithms"},
     {"link": "webviewjs.html", "text": "webviewjs"},
     {"link": "Neutralinojs.html", "text": "Neutralinojs"},
     {"link": "Electron.html", "text": "Electron"},
-    {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月上旬の雑記"},
-    {"link": "TeFWiki.html", "text": "TeFWiki"},
-    {"link": "MdImgr.html", "text": "MdImgr"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
-    {"link": "%E3%83%8F%E3%83%B3%E3%83%87%E3%82%A3%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%8A%E3%83%BC.html", "text": "ハンディクリーナー"},
-    {"link": "%E3%82%B0%E3%83%83%E3%82%BA%E9%96%A2%E9%80%A3.html", "text": "グッズ関連"},
-    {"link": "Home.html", "text": "Home"},
-    {"link": "Dio110.html", "text": "Dio110"},
    ]
 }
 ]
