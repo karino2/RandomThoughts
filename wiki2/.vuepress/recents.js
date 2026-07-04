7bfc75148fae86e6b6afe382958b506519af8108
Kazuma Arino <hogeika2@gmail.com>
2026-07-04 23:05:52 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E8%87%AA%E4%BD%9C%E3%82%A2%E3%83%97%E3%83%AA.html", "text": "自作アプリ"},
+    {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
+    {"link": "Neutralinojs.html", "text": "Neutralinojs"},
+    {"link": "MdImgr.html", "text": "MdImgr"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "JavaScript.html", "text": "JavaScript"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月上旬の雑記"},
-    {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92.html", "text": "機械学習"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "%E3%82%B5%E3%82%A4%E3%82%B3%E3%83%AD%E6%9C%AC.html", "text": "サイコロ本"},
@@ -13,9 +16,6 @@ export const recents = [
     {"link": "PRML.html", "text": "PRML"},
     {"link": "HMM.html", "text": "HMM"},
     {"link": "CoverAndThomas.html", "text": "CoverAndThomas"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91TheArtOfComputerProgramming.html", "text": "【書籍】TheArtOfComputerProgramming"},
-    {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E6%9C%AC.html", "text": "アルゴリズム本"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91IntroductionToAlgorithms.html", "text": "【書籍】IntroductionToAlgorithms"},
    ]
 }
 ]
