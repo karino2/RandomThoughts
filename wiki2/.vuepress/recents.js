a388562ba28ff09bf9b27b32aa512563a8dd5d84
Kazuma Arino <hogeika2@gmail.com>
2026-07-14 22:05:57 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -2,6 +2,8 @@ export const recents = [
  {text: "Recents:",
    children: [
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
+    {"link": "%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%842%E5%91%A8%E7%9B%AE.html", "text": "それっぽく描くコツ2周目"},
+    {"link": "%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%841%E5%91%A8%E7%9B%AE.html", "text": "それっぽく描くコツ1周目"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月中旬の雑記"},
     {"link": "Home.html", "text": "Home"},
@@ -14,8 +16,6 @@ export const recents = [
     {"link": "Neutralinojs.html", "text": "Neutralinojs"},
     {"link": "MdImgr.html", "text": "MdImgr"},
     {"link": "kickimgr.html", "text": "kickimgr"},
-    {"link": "%E3%83%8F%E3%83%B3%E3%83%87%E3%82%A3%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%8A%E3%83%BC.html", "text": "ハンディクリーナー"},
-    {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
    ]
 }
 ]
