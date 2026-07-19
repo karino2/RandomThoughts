f8d6f6b413ba5586811dc4ab9151787685b1bb32
Kazuma Arino <hogeika2@gmail.com>
2026-07-19 18:05:57 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E6%96%87%E5%AD%97%E5%88%97%E3%83%9E%E3%83%83%E3%83%81.html", "text": "文字列マッチ"},
     {"link": "SuffixArray.html", "text": "SuffixArray"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月中旬の雑記"},
     {"link": "Home.html", "text": "Home"},
@@ -9,7 +10,6 @@ export const recents = [
     {"link": "Rhinocs_%E3%82%BB%E3%83%83%E3%83%88%E3%82%A2%E3%83%83%E3%83%97.html", "text": "Rhinocs_セットアップ"},
     {"link": "SKK.html", "text": "SKK"},
     {"link": "Heap.html", "text": "Heap"},
-    {"link": "%E6%96%87%E5%AD%97%E5%88%97%E3%83%9E%E3%83%83%E3%83%81.html", "text": "文字列マッチ"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E6%9C%AC.html", "text": "アルゴリズム本"},
     {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "アルゴリズム"},
