81b0490999b2d87bc2bdc93672d78826b37cf2a0
Kazuma Arino <hogeika2@gmail.com>
2026-07-23 11:05:54 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91IntroductionToAlgorithms.html", "text": "【書籍】IntroductionToAlgorithms"},
+    {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "アルゴリズム"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
     {"link": "SuffixArray.html", "text": "SuffixArray"},
     {"link": "%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%842%E5%91%A8%E7%9B%AE.html", "text": "それっぽく描くコツ2周目"},
@@ -13,9 +15,7 @@ export const recents = [
     {"link": "Heap.html", "text": "Heap"},
     {"link": "%E6%8A%80%E8%A1%93%E7%9A%84%E3%81%AA%E3%83%A1%E3%83%A2.html", "text": "技術的なメモ"},
     {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E6%9C%AC.html", "text": "アルゴリズム本"},
-    {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "アルゴリズム"},
     {"link": "HMM.html", "text": "HMM"},
-    {"link": "Wiki%E3%81%A8%E6%89%8B%E6%9B%B8%E3%81%8D%E3%83%8E%E3%83%BC%E3%83%88%E3%81%AE%E8%9E%8D%E5%90%88.html", "text": "Wikiと手書きノートの融合"},
    ]
 }
 ]
