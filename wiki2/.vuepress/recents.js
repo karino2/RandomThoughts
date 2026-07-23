bb2a9f7bbd75cfc7eaa6c2085ebd4e9fb4f93b69
Kazuma Arino <hogeika2@gmail.com>
2026-07-24 00:05:56 +0900

    update recents

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "VuePress.html", "text": "VuePress"},
+    {"link": "Neutralinojs.html", "text": "Neutralinojs"},
+    {"link": "GithubPagesGallery.html", "text": "GithubPagesGallery"},
+    {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
     {"link": "%E3%83%A1%E3%83%A2%E3%82%92%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AB%E4%BE%9D%E5%AD%98%E3%81%95%E3%81%9B%E3%81%9F%E3%81%8F%E3%81%AA%E3%81%84.html", "text": "メモをアプリケーションに依存させたくない"},
     {"link": "Wiki%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%92%E6%89%B1%E3%81%86%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88.html", "text": "Wikiのリンクを扱うシェルスクリプト"},
@@ -12,10 +16,6 @@ export const recents = [
     {"link": "SAF.html", "text": "SAF"},
     {"link": "Home.html", "text": "Home"},
     {"link": "%E6%96%87%E5%AD%97%E5%88%97%E3%83%9E%E3%83%83%E3%83%81.html", "text": "文字列マッチ"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91IntroductionToAlgorithms.html", "text": "【書籍】IntroductionToAlgorithms"},
-    {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "アルゴリズム"},
-    {"link": "SuffixArray.html", "text": "SuffixArray"},
-    {"link": "Diff.html", "text": "Diff"},
    ]
 }
 ]
