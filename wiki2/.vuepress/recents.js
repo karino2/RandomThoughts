f23b9393459180d849e52c1eb9de79360841e46b
Kazuma Arino <hogeika2@gmail.com>
2026-07-25 01:05:57 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E5%85%83%E7%A5%96%E6%B0%B4%E7%8E%89%E6%9C%AC%E8%88%97.html", "text": "元祖水玉本舗"},
+    {"link": "RandomThoughts.html", "text": "RandomThoughts"},
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
     {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D.html", "text": "お絵描き"},
     {"link": "%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0.html", "text": "アルゴリズム"},
     {"link": "Home.html", "text": "Home"},
@@ -12,7 +13,6 @@ export const recents = [
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "Wiki%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%92%E6%89%B1%E3%81%86%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88.html", "text": "Wikiのリンクを扱うシェルスクリプト"},
     {"link": "VuePress.html", "text": "VuePress"},
-    {"link": "RandomThoughts.html", "text": "RandomThoughts"},
     {"link": "GithubPages%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%9F%E7%94%BB%E5%83%8F%E7%BD%AE%E3%81%8D%E5%A0%B4.html", "text": "GithubPagesを使った画像置き場"},
     {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "Mac.html", "text": "Mac"},
