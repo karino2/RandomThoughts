c214b7c65e0aed039c5d2961664313e41e1e774b
Kazuma Arino <hogeika2@gmail.com>
2026-07-24 12:25:14 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,14 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Wiki%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%92%E6%89%B1%E3%81%86%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88.html", "text": "Wikiのリンクを扱うシェルスクリプト"},
     {"link": "VuePress.html", "text": "VuePress"},
-    {"link": "Mac.html", "text": "Mac"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
+    {"link": "Mac.html", "text": "Mac"},
     {"link": "Neutralinojs.html", "text": "Neutralinojs"},
     {"link": "GithubPagesGallery.html", "text": "GithubPagesGallery"},
     {"link": "GithubPages.html", "text": "GithubPages"},
     {"link": "%E3%83%A1%E3%83%A2%E3%82%92%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AB%E4%BE%9D%E5%AD%98%E3%81%95%E3%81%9B%E3%81%9F%E3%81%8F%E3%81%AA%E3%81%84.html", "text": "メモをアプリケーションに依存させたくない"},
-    {"link": "Wiki%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%92%E6%89%B1%E3%81%86%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88.html", "text": "Wikiのリンクを扱うシェルスクリプト"},
     {"link": "TeFWiki.html", "text": "TeFWiki"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "Wiki%E3%81%A8howm%E3%81%A8%E3%83%A1%E3%83%A2%E3%83%84%E3%83%BC%E3%83%AB.html", "text": "Wikiとhowmとメモツール"},
