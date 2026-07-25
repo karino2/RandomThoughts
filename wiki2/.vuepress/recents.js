df35588971cbac15c92cc83abe611b444f750e4c
Kazuma Arino <hogeika2@gmail.com>
2026-07-25 22:05:23 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -2,6 +2,7 @@ export const recents = [
  {text: "Recents:",
    children: [
     {"link": "%E5%85%83%E7%A5%96%E6%B0%B4%E7%8E%89%E6%9C%AC%E8%88%97.html", "text": "元祖水玉本舗"},
+    {"link": "%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%842%E5%91%A8%E7%9B%AE.html", "text": "それっぽく描くコツ2周目"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
     {"link": "SuffixArray.html", "text": "SuffixArray"},
     {"link": "EnhancedSuffixArray.html", "text": "EnhancedSuffixArray"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "Home.html", "text": "Home"},
     {"link": "%E3%81%9D%E3%81%AE%E3%81%86%E3%81%A1%E3%82%84%E3%82%8A%E3%81%9F%E3%81%84%E4%BA%8B.html", "text": "そのうちやりたい事"},
     {"link": "Wiki%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%92%E6%89%B1%E3%81%86%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88.html", "text": "Wikiのリンクを扱うシェルスクリプト"},
-    {"link": "VuePress.html", "text": "VuePress"},
    ]
 }
 ]
