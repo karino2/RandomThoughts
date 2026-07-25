08debf6bb7526716de265e2888a091111034ff2e
Kazuma Arino <hogeika2@gmail.com>
2026-07-26 01:15:50 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,12 +1,12 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SuffixArray.html", "text": "SuffixArray"},
     {"link": "%E5%85%83%E7%A5%96%E6%B0%B4%E7%8E%89%E6%9C%AC%E8%88%97.html", "text": "元祖水玉本舗"},
     {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D.html", "text": "お絵描き"},
     {"link": "%E6%B0%B4%E7%8E%89%E6%87%90%E3%82%B2%E3%83%BC%E5%A4%9C%E8%A9%B1.html", "text": "水玉懐ゲー夜話"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
     {"link": "%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%842%E5%91%A8%E7%9B%AE.html", "text": "それっぽく描くコツ2周目"},
-    {"link": "SuffixArray.html", "text": "SuffixArray"},
     {"link": "EnhancedSuffixArray.html", "text": "EnhancedSuffixArray"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "【書籍】原論文から解き明かす生成AI"},
     {"link": "Heap.html", "text": "Heap"},
