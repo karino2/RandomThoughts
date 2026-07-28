a4887db70d96869f8f21150d3bcdae146c003260
Kazuma Arino <hogeika2@gmail.com>
2026-07-28 21:05:25 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,9 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "TeFWiki.html", "text": "TeFWiki"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92.html", "text": "機械学習"},
     {"link": "%E3%82%AF%E3%83%AD%E3%82%B9%E3%82%A8%E3%83%B3%E3%83%88%E3%83%AD%E3%83%94%E3%83%BC.html", "text": "クロスエントロピー"},
     {"link": "MacKay.html", "text": "MacKay"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "CoverAndThomas.html", "text": "CoverAndThomas"},
     {"link": "%E6%B0%B4%E7%8E%89%E6%87%90%E3%82%B2%E3%83%BC%E5%A4%9C%E8%A9%B1.html", "text": "水玉懐ゲー夜話"},
     {"link": "%E6%95%B0%E5%AD%A6.html", "text": "数学"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
    ]
 }
 ]
