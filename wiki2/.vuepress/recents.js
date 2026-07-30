c8049484991304cd055d74c8aa461d854a3b876e
Kazuma Arino <hogeika2@gmail.com>
2026-07-30 23:05:22 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,14 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E6%B0%B4%E7%8E%89%E6%87%90%E3%82%B2%E3%83%BC%E5%A4%9C%E8%A9%B1.html", "text": "水玉懐ゲー夜話"},
+    {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
     {"link": "%E4%B8%80%E4%BA%BA%E8%AA%AD%E6%9B%B8%E4%BC%9A%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "一人読書会ライブ"},
-    {"link": "%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "ライブ"},
     {"link": "2026%E5%B9%B47%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年7月下旬の雑記"},
+    {"link": "%E6%B0%B4%E7%8E%89%E6%87%90%E3%82%B2%E3%83%BC%E5%A4%9C%E8%A9%B1.html", "text": "水玉懐ゲー夜話"},
+    {"link": "%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "ライブ"},
     {"link": "%E3%83%87%E3%83%BC%E3%82%BF%E5%9C%A7%E7%B8%AE.html", "text": "データ圧縮"},
     {"link": "SentencePiece.html", "text": "SentencePiece"},
     {"link": "Heap.html", "text": "Heap"},
-    {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
     {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E5%85%A5%E9%96%80.html", "text": "型システム入門"},
     {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0.html", "text": "型システム"},
     {"link": "Nushell.html", "text": "Nushell"},
