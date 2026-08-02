f283cb65fb6dad833a31f49f2bec7bdcca2aab3c
Kazuma Arino <hogeika2@gmail.com>
2026-08-02 21:05:24 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E6%B0%B4%E7%8E%89%E6%87%90%E3%82%B2%E3%83%BC%E5%A4%9C%E8%A9%B1.html", "text": "水玉懐ゲー夜話"},
     {"link": "Go%E3%81%A8WebView.html", "text": "GoとWebView"},
     {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月上旬の雑記"},
     {"link": "Go.html", "text": "Go"},
     {"link": "fyne.html", "text": "fyne"},
-    {"link": "%E6%B0%B4%E7%8E%89%E6%87%90%E3%82%B2%E3%83%BC%E5%A4%9C%E8%A9%B1.html", "text": "水玉懐ゲー夜話"},
     {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "型システムのしくみ"},
     {"link": "TypeScript.html", "text": "TypeScript"},
     {"link": "RandomThoughts.html", "text": "RandomThoughts"},
