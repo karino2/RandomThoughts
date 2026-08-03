6a6b05c37d8f9b4727457a1d2aaf4d056359fb57
Kazuma Arino <hogeika2@gmail.com>
2026-08-03 23:05:25 +0900

    update recents, backlink

--- wiki2/.vuepress/components/backlinkData.js
+++ wiki2/.vuepress/components/backlinkData.js
@@ -199,6 +199,7 @@ export const backlinkData = {
 "【書籍】ギャノング生理学.md",
 ],
 "SurfLog.md": [
+"2026年8月上旬の雑記.md",
 "Home.md",
 "SurfLog_202504から202512まで.md",
 "SurfLog_202512から202603まで.md",
--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月上旬の雑記"},
     {"link": "TypeScript.html", "text": "TypeScript"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "ExploringTypeScript.html", "text": "ExploringTypeScript"},
-    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月上旬の雑記"},
     {"link": "Kakito.html", "text": "Kakito"},
     {"link": "BOOX.html", "text": "BOOX"},
     {"link": "MdImgr.html", "text": "MdImgr"},
