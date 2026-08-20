1239aec9882ef3562e2df6858a1cf84abe7f062c
Kazuma Arino <hogeika2@gmail.com>
2026-08-20 13:05:38 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月中旬の雑記"},
     {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "型システムのしくみ"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91ModernCompilerImplementationInML.html", "text": "【書籍】ModernCompilerImplementationInML"},
     {"link": "%E8%99%8E%E6%9C%AC.html", "text": "虎本"},
-    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月中旬の雑記"},
     {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D%E9%9B%91%E8%AB%87%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "お絵描き雑談ライブ"},
     {"link": "%E4%B8%80%E4%BA%BA%E8%AA%AD%E6%9B%B8%E4%BC%9A%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "一人読書会ライブ"},
     {"link": "%E3%82%B9%E3%83%9E%E3%83%9B.html", "text": "スマホ"},
