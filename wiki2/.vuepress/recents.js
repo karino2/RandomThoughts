9592465cfd7f2703a60761c68d250899630d6513
Kazuma Arino <hogeika2@gmail.com>
2026-08-22 00:05:37 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
     {"link": "%E6%A9%9F%E6%A2%B0%E7%BF%BB%E8%A8%B3.html", "text": "機械翻訳"},
     {"link": "%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92.html", "text": "機械学習"},
@@ -10,12 +12,10 @@ export const recents = [
     {"link": "ConvS2S.html", "text": "ConvS2S"},
     {"link": "ConvolutionalSequenceToSequenceLearning.html", "text": "ConvolutionalSequenceToSequenceLearning"},
     {"link": "AttentionIsAllYouNeed.html", "text": "AttentionIsAllYouNeed"},
-    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "型システムのしくみ"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91ModernCompilerImplementationInML.html", "text": "【書籍】ModernCompilerImplementationInML"},
     {"link": "%E8%99%8E%E6%9C%AC.html", "text": "虎本"},
     {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D%E9%9B%91%E8%AB%87%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "お絵描き雑談ライブ"},
-    {"link": "%E4%B8%80%E4%BA%BA%E8%AA%AD%E6%9B%B8%E4%BC%9A%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "一人読書会ライブ"},
    ]
 }
 ]
