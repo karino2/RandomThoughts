cdca6a503c40e201092451efedddd40a32fc2cea
Kazuma Arino <hogeika2@gmail.com>
2026-08-22 23:05:33 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "WikiName%E3%81%AE%E4%BD%9C%E3%82%8A%E6%96%B9.html", "text": "WikiNameの作り方"},
     {"link": "SurfLog.html", "text": "SurfLog"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "ConvolutionalSequenceToSequenceLearning.html", "text": "ConvolutionalSequenceToSequenceLearning"},
     {"link": "AttentionIsAllYouNeed.html", "text": "AttentionIsAllYouNeed"},
     {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "型システムのしくみ"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91ModernCompilerImplementationInML.html", "text": "【書籍】ModernCompilerImplementationInML"},
    ]
 }
 ]
