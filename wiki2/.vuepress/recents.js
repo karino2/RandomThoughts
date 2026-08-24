363b71a8a8cbabe19134721885dcea5c08270ad5
Kazuma Arino <hogeika2@gmail.com>
2026-08-24 21:05:36 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
     {"link": "WikiName%E3%81%AE%E4%BD%9C%E3%82%8A%E6%96%B9.html", "text": "WikiNameの作り方"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
-    {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
     {"link": "%E6%A9%9F%E6%A2%B0%E7%BF%BB%E8%A8%B3.html", "text": "機械翻訳"},
     {"link": "%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92.html", "text": "機械学習"},
