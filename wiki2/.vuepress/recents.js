4014c7be162a374409d410ee980c949679a3ca33
Kazuma Arino <hogeika2@gmail.com>
2026-08-25 21:05:37 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D%E9%9B%91%E8%AB%87%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "お絵描き雑談ライブ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "ConvS2S.html", "text": "ConvS2S"},
     {"link": "ConvolutionalSequenceToSequenceLearning.html", "text": "ConvolutionalSequenceToSequenceLearning"},
     {"link": "AttentionIsAllYouNeed.html", "text": "AttentionIsAllYouNeed"},
-    {"link": "%E5%9E%8B%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%81%AE%E3%81%97%E3%81%8F%E3%81%BF.html", "text": "型システムのしくみ"},
    ]
 }
 ]
