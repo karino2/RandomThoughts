fd2ea8ca81a403ad047bf18961ba59c0087d82a0
Kazuma Arino <hogeika2@gmail.com>
2026-08-26 23:05:37 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D%E9%9B%91%E8%AB%87%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "お絵描き雑談ライブ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D%E9%9B%91%E8%AB%87%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "お絵描き雑談ライブ"},
     {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E3%83%8E%E3%83%BC%E3%83%88.html", "text": "ノート"},
     {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E8%88%88%E4%BA%A1%E3%81%AE%E4%B8%96%E7%95%8C%E5%8F%B2%E3%80%81%E3%82%B7%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%89%E3%81%A8%E5%94%90%E5%B8%9D%E5%9B%BD.html", "text": "【書籍】興亡の世界史、シルクロードと唐帝国"},
