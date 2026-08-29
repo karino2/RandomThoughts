848ece915aa7b54a77e95156407aa910730f3788
Kazuma Arino <hogeika2@gmail.com>
2026-08-29 16:05:41 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "ConvS2S.html", "text": "ConvS2S"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "%E3%81%8A%E7%B5%B5%E6%8F%8F%E3%81%8D%E9%9B%91%E8%AB%87%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "お絵描き雑談ライブ"},
@@ -13,7 +14,6 @@ export const recents = [
     {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
     {"link": "Transformer.html", "text": "Transformer"},
     {"link": "Home.html", "text": "Home"},
-    {"link": "ConvS2S.html", "text": "ConvS2S"},
     {"link": "ConvolutionalSequenceToSequenceLearning.html", "text": "ConvolutionalSequenceToSequenceLearning"},
     {"link": "AttentionIsAllYouNeed.html", "text": "AttentionIsAllYouNeed"},
    ]
