a9b0c358934e4adadb5f3f27971bdebeb46d96e7
Kazuma Arino <hogeika2@gmail.com>
2026-08-31 12:38:27 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "クイックスケッチライブ"},
     {"link": "SQuAD.html", "text": "SQuAD"},
     {"link": "OnPositionEmbeddingsInBERT.html", "text": "OnPositionEmbeddingsInBERT"},
     {"link": "2026%E5%B9%B48%E6%9C%88%E4%B8%8B%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年8月下旬の雑記"},
     {"link": "BERT.html", "text": "BERT"},
-    {"link": "%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "クイックスケッチライブ"},
     {"link": "PositionEmbeddings.html", "text": "PositionEmbeddings"},
     {"link": "%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "ライブ"},
     {"link": "%E3%81%9F%E3%81%A6%E3%81%AA%E3%81%8B%E6%B5%81%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "たてなか流クイックスケッチ"},
