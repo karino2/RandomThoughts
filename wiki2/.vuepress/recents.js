8b3b70723fe6c68515182bafc10f74eb624dc87e
Kazuma Arino <hogeika2@gmail.com>
2026-09-01 19:05:42 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,10 +1,10 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Transformer.html", "text": "Transformer"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月上旬の雑記"},
     {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
     {"link": "Karino2DIYApp.html", "text": "Karino2DIYApp"},
-    {"link": "Transformer.html", "text": "Transformer"},
     {"link": "PositionEmbeddings.html", "text": "PositionEmbeddings"},
     {"link": "Home.html", "text": "Home"},
     {"link": "WhatDoPositionEmbeddingsLearn.html", "text": "WhatDoPositionEmbeddingsLearn"},
