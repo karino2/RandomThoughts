11e1b3f4798404c116061959062611417c5ae690
Kazuma Arino <hogeika2@gmail.com>
2026-09-02 18:05:43 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,13 +1,14 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
+    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "Transformer.html", "text": "Transformer"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月上旬の雑記"},
     {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "クイックスケッチライブ"},
     {"link": "PositionEmbeddings.html", "text": "PositionEmbeddings"},
-    {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
     {"link": "Karino2DIYApp.html", "text": "Karino2DIYApp"},
     {"link": "Home.html", "text": "Home"},
     {"link": "WhatDoPositionEmbeddingsLearn.html", "text": "WhatDoPositionEmbeddingsLearn"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "GLUE.html", "text": "GLUE"},
     {"link": "BERT.html", "text": "BERT"},
     {"link": "RoPE.html", "text": "RoPE"},
-    {"link": "%E3%80%90%E6%9B%B8%E7%B1%8D%E3%80%91%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%92%E3%81%9D%E3%82%8C%E3%81%A3%E3%81%BD%E3%81%8F%E6%8F%8F%E3%81%8F%E3%82%B3%E3%83%84.html", "text": "【書籍】イラストをそれっぽく描くコツ"},
    ]
 }
 ]
