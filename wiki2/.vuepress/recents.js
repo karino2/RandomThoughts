53fc3411e0588235fa31ff383ecd6eccc88dd195
Kazuma Arino <hogeika2@gmail.com>
2026-09-04 21:05:44 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,16 +1,16 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "SurfLog.html", "text": "SurfLog"},
+    {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月上旬の雑記"},
     {"link": "%E6%B7%B1%E5%B1%A4%E5%AD%A6%E7%BF%92.html", "text": "深層学習"},
     {"link": "Transformer.html", "text": "Transformer"},
     {"link": "LayerNormalization.html", "text": "LayerNormalization"},
     {"link": "BatchNormalization.html", "text": "BatchNormalization"},
-    {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月上旬の雑記"},
     {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
     {"link": "%E3%82%BB%E3%83%AB%E3%83%95%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "セルフアテンション"},
     {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
     {"link": "Wiki%E3%81%A8%E6%89%8B%E6%9B%B8%E3%81%8D%E3%83%8E%E3%83%BC%E3%83%88%E3%81%AE%E8%9E%8D%E5%90%88.html", "text": "Wikiと手書きノートの融合"},
-    {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "PositionEmbeddings.html", "text": "PositionEmbeddings"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "クイックスケッチライブ"},
