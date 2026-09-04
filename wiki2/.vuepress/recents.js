a1bdc0f91eab58559043b503147ae9d6886fd0eb
Kazuma Arino <hogeika2@gmail.com>
2026-09-04 22:05:42 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%81%9F%E3%81%A6%E3%81%AA%E3%81%8B%E6%B5%81%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "たてなか流クイックスケッチ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月上旬の雑記"},
     {"link": "%E6%B7%B1%E5%B1%A4%E5%AD%A6%E7%BF%92.html", "text": "深層学習"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "クイックスケッチライブ"},
     {"link": "Karino2DIYApp.html", "text": "Karino2DIYApp"},
-    {"link": "Home.html", "text": "Home"},
    ]
 }
 ]
