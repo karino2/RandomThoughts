3820a862bc1ea0cbd328807b4917013e7ec4bd69
Kazuma Arino <hogeika2@gmail.com>
2026-09-05 11:05:41 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,11 +1,11 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Transformer.html", "text": "Transformer"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%8A%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月上旬の雑記"},
     {"link": "%E3%81%9F%E3%81%A6%E3%81%AA%E3%81%8B%E6%B5%81%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B1%E3%83%83%E3%83%81.html", "text": "たてなか流クイックスケッチ"},
     {"link": "SurfLog.html", "text": "SurfLog"},
     {"link": "%E6%B7%B1%E5%B1%A4%E5%AD%A6%E7%BF%92.html", "text": "深層学習"},
-    {"link": "Transformer.html", "text": "Transformer"},
     {"link": "LayerNormalization.html", "text": "LayerNormalization"},
     {"link": "BatchNormalization.html", "text": "BatchNormalization"},
     {"link": "%E5%8E%9F%E8%AB%96%E6%96%87%E3%81%8B%E3%82%89%E8%A7%A3%E3%81%8D%E6%98%8E%E3%81%8B%E3%81%99%E7%94%9F%E6%88%90AI.html", "text": "原論文から解き明かす生成AI"},
