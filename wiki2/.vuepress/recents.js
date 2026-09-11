733ea14f0be5f267457619c560bed0a84a56be99
Kazuma Arino <hogeika2@gmail.com>
2026-09-11 13:05:43 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "%E3%82%B2%E3%83%BC%E3%83%A0.html", "text": "ゲーム"},
     {"link": "Mac.html", "text": "Mac"},
     {"link": "launchpick.html", "text": "launchpick"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月中旬の雑記"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
     {"link": "SoftMax.html", "text": "SoftMax"},
     {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%A8%E3%82%BB%E3%83%AB%E3%83%95%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンションとセルフアテンション"},
-    {"link": "Transformer.html", "text": "Transformer"},
    ]
 }
 ]
