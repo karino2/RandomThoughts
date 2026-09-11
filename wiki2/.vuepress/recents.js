96f6daa1f64363118ad0294d8b4ea4ab90a5cf22
Kazuma Arino <hogeika2@gmail.com>
2026-09-11 19:05:43 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,6 +1,7 @@
 export const recents = [
  {text: "Recents:",
    children: [
+    {"link": "Rhinocs_%E4%BB%95%E6%A7%98%E6%A4%9C%E8%A8%8E.html", "text": "Rhinocs_仕様検討"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月中旬の雑記"},
     {"link": "%E3%82%B2%E3%83%BC%E3%83%A0.html", "text": "ゲーム"},
@@ -15,7 +16,6 @@ export const recents = [
     {"link": "SurfLog_202604%E3%81%8B%E3%82%89202608%E3%81%BE%E3%81%A7.html", "text": "SurfLog_202604から202608まで"},
     {"link": "%E3%82%BB%E3%83%AB%E3%83%95%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "セルフアテンション"},
     {"link": "%E3%82%A2%E3%83%86%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%B3.html", "text": "アテンション"},
-    {"link": "SoftMax.html", "text": "SoftMax"},
    ]
 }
 ]
