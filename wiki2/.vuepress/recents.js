0d26cced985481397e7c3800f4139d064a014183
Kazuma Arino <hogeika2@gmail.com>
2026-09-13 12:05:47 +0900

    update recents, backlink

--- wiki2/.vuepress/recents.js
+++ wiki2/.vuepress/recents.js
@@ -1,8 +1,8 @@
 export const recents = [
  {text: "Recents:",
    children: [
-    {"link": "LayerNorm%E3%81%A8Transformer%E3%83%96%E3%83%AD%E3%83%83%E3%82%AF.html", "text": "LayerNormとTransformerブロック"},
     {"link": "2026%E5%B9%B49%E6%9C%88%E4%B8%AD%E6%97%AC%E3%81%AE%E9%9B%91%E8%A8%98.html", "text": "2026年9月中旬の雑記"},
+    {"link": "LayerNorm%E3%81%A8Transformer%E3%83%96%E3%83%AD%E3%83%83%E3%82%AF.html", "text": "LayerNormとTransformerブロック"},
     {"link": "%E4%B8%80%E4%BA%BA%E8%AA%AD%E6%9B%B8%E4%BC%9A%E3%83%A9%E3%82%A4%E3%83%96.html", "text": "一人読書会ライブ"},
     {"link": "Rhinocs.html", "text": "Rhinocs"},
     {"link": "LayerNormalization.html", "text": "LayerNormalization"},
